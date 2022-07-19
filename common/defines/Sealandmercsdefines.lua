090525c206337aa3572542d854e8eae1b440c531
BulwiakAcademy <kazikgemba@gmail.com>
2022-07-19 12:19:02 +0200

    july update

--- common/defines/Sealandmercsdefines.lua
+++ common/defines/Sealandmercsdefines.lua
@@ -1,10 +1,10 @@
 -- USES MANPOWER TO REIFORCE. THAT SUCKS.
---NDefines.NMilitary.CAWA_STARTING_STRENGTH = 1					-- Starting strength of a cawa regiment, 1.0 being full strength
---NDefines.NMilitary.CAWA_STARTING_MORALE = 0					-- Starting morale of a cawa regiment, 1.0 being full strength
---NDefines.NMilitary.CREATE_CAWA_MIL_COST = 0						-- Cost to create Cawa (per unit)
---NDefines.NMilitary.CAWA_USES_CONSTRUCTION = 0
+NDefines.NMilitary.CAWA_STARTING_STRENGTH = 1					-- Starting strength of a cawa regiment, 1.0 being full strength
+NDefines.NMilitary.CAWA_STARTING_MORALE = 0					-- Starting morale of a cawa regiment, 1.0 being full strength
+NDefines.NMilitary.CREATE_CAWA_MIL_COST = 0						-- Cost to create Cawa (per unit)
+NDefines.NMilitary.CAWA_USES_CONSTRUCTION = 0
 
-NDefines.NCountry.CORRUPTION_FROM_BANNERS = 0
+NDefines.NCountry.CORRUPTION_FROM_BANNERS = 2
 NDefines.NMilitary.BANNER_DISCIPLINE_BONUS = -0.05
 NDefines.NMilitary.BANNER_MANPOWER_USAGE = 0
 NDefines.NMilitary.BANNER_REINFORCE_SPEED = 0
@@ -12,3 +12,6 @@ NDefines.NMilitary.BANNER_MAINTENANCE_COST = 4.0
 NDefines.NMilitary.BANNER_AGE_MULTIPLIER = 0
 NDefines.NMilitary.BANNER_STARTING_STRENGTH = 0.1					-- Starting strength of a banner regiment, 1.0 being full strength
 NDefines.NMilitary.BANNER_USES_CONSTRUCTION = 0
+
+NDefines.NMilitary.CREATE_JANISSARIES_MIL_COST = 7
+NDefines.NMilitary.JANISSARIES_USES_CONSTRUCTION = 1
