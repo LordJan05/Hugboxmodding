31a685937f5a588e10279d656dceaddb67aaa2b6
BulwiakAcademy <kazikgemba@gmail.com>
2022-10-29 18:53:27 +0200

    october update

--- common/defines/Sealandmercsdefines.lua
+++ common/defines/Sealandmercsdefines.lua
@@ -5,13 +5,25 @@ NDefines.NMilitary.CREATE_CAWA_MIL_COST = 0						-- Cost to create Cawa (per uni
 NDefines.NMilitary.CAWA_USES_CONSTRUCTION = 0
 
 NDefines.NCountry.CORRUPTION_FROM_BANNERS = 2
-NDefines.NMilitary.BANNER_DISCIPLINE_BONUS = -0.05
-NDefines.NMilitary.BANNER_MANPOWER_USAGE = 0
-NDefines.NMilitary.BANNER_REINFORCE_SPEED = 0
-NDefines.NMilitary.BANNER_MAINTENANCE_COST = 4.0
+--NDefines.NMilitary.BANNER_DISCIPLINE_BONUS = -0.05
+--NDefines.NMilitary.BANNER_MANPOWER_USAGE = 0
+--NDefines.NMilitary.BANNER_REINFORCE_SPEED = 0
+--NDefines.NMilitary.BANNER_MAINTENANCE_COST = 4.0
 NDefines.NMilitary.BANNER_AGE_MULTIPLIER = 0
 NDefines.NMilitary.BANNER_STARTING_STRENGTH = 0.1					-- Starting strength of a banner regiment, 1.0 being full strength
 NDefines.NMilitary.BANNER_USES_CONSTRUCTION = 0
 
 NDefines.NMilitary.CREATE_JANISSARIES_MIL_COST = 7
 NDefines.NMilitary.JANISSARIES_USES_CONSTRUCTION = 1
+
+
+-- NEW STUFF WITH SPECIAL UNITS, KEEPING HERE TO SEE:
+--NDefines.NMilitary.CAROLEAN_BASE_COST_MODIFIER = 1.0
+--NDefines.NMilitary.CAROLEAN_USES_CONSTRUCTION = 1
+
+--NDefines.NMilitary.HUSSARS_BASE_COST_MODIFIER = 1.0
+--NDefines.NMilitary.HUSSARS_MANPOWER_COST_MODIFIER = 1.0
+--NDefines.NMilitary.HUSSARS_STARTING_STRENGTH = 1.0
+--NDefines.NMilitary.HUSSARS_STARTING_MORALE = 0.1
+--NDefines.NMilitary.CREATE_HUSSARS_MIL_COST = 0
+--NDefines.NMilitary.HUSSARS_USES_CONSTRUCTION = 1
--- descriptor.mod
+++ descriptor.mod
@@ -1,8 +1,8 @@
-version="1.8"
+version="1.9"
 tags={
 	"Balance"
 }
 name="Zlewikk-MJL Multiplayer Rebalance Pack"
 picture="thumbnail.png"
-supported_version="1.33.*"
+supported_version="1.34.*"
 remote_file_id="2235590207"
