02d27eaa8881ccc2b7a74e7936b974ecc88bb8a2
BulwiakAcademy <kazikgemba@gmail.com>
2022-11-16 16:21:55 +0100

    hotfix

--- common/defines/Sealandmercsdefines.lua
+++ common/defines/Sealandmercsdefines.lua
@@ -1,7 +1,7 @@
 -- USES MANPOWER TO REIFORCE. THAT SUCKS.
 NDefines.NMilitary.CAWA_STARTING_STRENGTH = 1					-- Starting strength of a cawa regiment, 1.0 being full strength
 NDefines.NMilitary.CAWA_STARTING_MORALE = 0					-- Starting morale of a cawa regiment, 1.0 being full strength
-NDefines.NMilitary.CREATE_CAWA_MIL_COST = 0						-- Cost to create Cawa (per unit)
+NDefines.NMilitary.CREATE_CAWA_MIL_COST = 7						-- Cost to create Cawa (per unit)
 NDefines.NMilitary.CAWA_USES_CONSTRUCTION = 0
 
 NDefines.NCountry.CORRUPTION_FROM_BANNERS = 2
